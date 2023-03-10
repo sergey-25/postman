9d8f68bea54585463c4150e8311b8912deafc037
S.Kurylenko <73819378+sergey-25@users.noreply.github.com>
2023-03-10 11:18:00 +0200

    dep

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ app.post('/write', (req, res) => {
 });
 
 
-app.listen(process.env.PORT, () => {
+app.listen(3030, () => {
     console.log('ResponsesToFile App is listening now! Send them requests my way!');
     console.log(`Data is being stored at location: ${path.join(process.cwd(), folderPath)}`);
 });
